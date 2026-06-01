c1b3dd2128a12c8609f6e2f7804248f9b624caf8
Tyler Krys <tylerkrys@gmail.com>
2026-05-31 20:49:24 -0700

    Use src/** glob pattern for Vitest config exclude

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["__tests__/importTests.test.ts"],
-    exclude: ["src"],
+    exclude: ["src/**"],
   },
 });
