78ff866dc1941d721ca5f99365f0d53c83f9285b
Tyler Krys <tylerkrys@gmail.com>
2026-05-31 20:34:59 -0700

    Add vitest.config.ts for v-k-b distributed files

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+// vitest.config.ts
+// ----------------
+// Used for testing the `v-k-b` package distributed files.
+// Individual front-end components in `src` are out of scope.
+
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    include: [],
+    exclude: ["src"],
+  },
+});
